318cb734e0825ff62d1d0f6cc8b18bee14a22bf1
Yuri S. <fudoshiki.ari@gmail.com>
2025-11-22 11:30:26 +0500

    Remove Fallback to Phoenix default decoder

--- priv/static/phoenix_socket_bert.js
+++ priv/static/phoenix_socket_bert.js
@@ -1,15 +1,8 @@
 "use strict";
 
 import { decode as bertDecode } from "./bert";
-import { Socket } from "phoenix";
-
-const { defaultDecoder } = new Socket();
 
 export const decode = (rawPayload, callback) => {
-  try {
-    let [join_ref, ref, topic, event, payload] = bertDecode(rawPayload);
-    return callback({ join_ref, ref, topic, event, payload });
-  } catch (error) {
-    return defaultDecoder(rawPayload, callback);
-  }
+  let [join_ref, ref, topic, event, payload] = bertDecode(rawPayload);
+  return callback({ join_ref, ref, topic, event, payload });
 };
